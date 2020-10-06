ab8bfa14d0aaa13eecc0e20374bec7ecc20a8dd6
Jay Wang <jaywang007@yahoo.com>
2020-10-05 21:40:15 -0500

    function and component

--- src/index.js
+++ src/index.js
@@ -7,16 +7,14 @@ let city = "Madrid"
 function Hello(){
   return (
     <div>
-      <h1>Welcome to React!</h1>
-      <p>Let's build something fun.</p>
+      <h1 id="heading" className="cool-text">
+        Hello from {city} 
+      </h1>
     </div>
   )
 }
 
 ReactDOM.render(
-  // <h1 id="heading" className="cool-text">
-  //   Hello from {city} 
-  // </h1>,
   <Hello />,
   document.getElementById('root')
 );
