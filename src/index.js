a040c68f836ad435ecd48d1e255a261866b7ca60
Jay Wang <jaywang007@yahoo.com>
2020-10-05 21:36:25 -0500

    played with React component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+let city = "Madrid"
+
+function Hello(){
+  return (
+    <div>
+      <h1>Welcome to React!</h1>
+      <p>Let's build something fun.</p>
+    </div>
+  )
+}
+
 ReactDOM.render(
-  <ul>
-    <li>Hot dogs</li>
-    <li>Hamburgers</li>
-    <li>Shakes</li>
-    <li>Fries</li>
-  </ul>,
+  // <h1 id="heading" className="cool-text">
+  //   Hello from {city} 
+  // </h1>,
+  <Hello />,
   document.getElementById('root')
 );
